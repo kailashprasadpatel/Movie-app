78a50d7ba7841da11fbccd6750c3517f244bbca0
Kailashprasd <kp350722@gmail.com>
2023-12-23 16:36:02 +0530

    movie app

---
+++ src/moviesData.js
@@ -0,0 +1,82 @@
+export const movies = [{
+    title: " Avengers Endgaem",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster: "https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0,0,540,810&width=480",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+
+    {
+    title: "Ironman2",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster: "https://i.pinimg.com/474x/6b/7a/94/6b7a9457ee6a5252c98e4edb2fee8548.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+    {
+    title: " Avengers Age of Ultron",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.4,
+    poster: "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+    {
+        title: "Doctor Strange",
+        plot: "Plot of the movie is related to super natural powers",
+        price: 199,
+        rating: 8.8,
+        poster: "https://m.media-amazon.com/images/M/MV5BNWM0ZGJlMzMtZmYwMi00NzI3LTgzMzMtNjMzNjliNDRmZmFlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg",
+        stars: 0,
+        fav:false,
+        isInCart:false
+        },
+        {
+            title: "Black Widow",
+            plot: "Plot of the movie is related to super natural powers",
+            price: 199,
+            rating: 8.9,
+            poster: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+            stars: 0,
+            fav:false,
+            isInCart:false
+            },
+            {
+                title: "Ant Man",
+                plot: "Plot of the movie is related to super natural powers",
+                price: 199,
+                rating: 8.5,
+                poster: "https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_QL75_UX190_CR0,0,190,281_.jpg",
+                stars: 0,
+                fav:false,
+                isInCart:false
+                },
+                {
+                    title: "Shangchi",
+                    plot: "Plot of the movie is related to super natural powers",
+                    price: 199,
+                    rating: 8.0,
+                    poster: "https://m.media-amazon.com/images/M/MV5BNTliYjlkNDQtMjFlNS00NjgzLWFmMWEtYmM2Mzc2Zjg3ZjEyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+                    stars: 0,
+                    fav:false,
+                    isInCart:false
+                    },
+                    {
+                        title: "Thor Love and Thunder",
+                        plot: "Plot of the movie is related to super natural powers",
+                        price: 199,
+                        rating: 8.7,
+                        poster: "https://m.media-amazon.com/images/M/MV5BYmMxZWRiMTgtZjM0Ny00NDQxLWIxYWQtZDdlNDNkOTEzYTdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+                        stars: 0,
+                        fav:false,
+                        isInCart:false
+                        }
+]
